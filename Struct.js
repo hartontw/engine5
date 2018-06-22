bb895597f9aa195be5d4de58a27a08473123a2f7
Harton <hartontw@gmail.com>
2018-06-22 17:47:25 +0200

    First structures

---
+++ Struct.js
@@ -0,0 +1,37 @@
+
+class Struct extends Object
+{
+	eq(other)
+	{
+		for(property in this)
+		{
+			if (!other.hasOwnProperty(property) || this[property] != other[property])
+				return false;
+		}
+		
+		return true;
+	}
+	
+	seq(other)
+	{
+		if (typeof other != typeof this) return false;
+
+		for(property in this)
+		{
+			if (this[property] !== other[property])
+				return false;
+		}
+			
+		return true;
+	}
+	
+	uneq(other)
+	{
+		return !this.eq(other);
+	}
+	
+	suneq(other)
+	{
+		return !this.seq(other);
+	}
+}
